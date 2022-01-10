9b56f9821382fa7579183f902bbc191939c4ab38
pluhar.vit@seznam.cz <pluhar.vit@seznam.cz>
2022-01-10 02:11:06 +0100

    data update

--- src/components/PlanningTool.js
+++ src/components/PlanningTool.js
@@ -18,15 +18,179 @@ const keys = {
 };
 
 export default class PlanningTool extends Component {
+
+  buildData = (data, groupsMap, items, level, parentId) => {
+    for (const item of data) {
+      if (!groupsMap.has(item.resource.id)) {
+        groupsMap.set(item.resource.id, {
+          id: groupsMap.size,
+          title: item.resource ? item.resource.title : '',
+          rightTitle: 'right title',
+          hasChildren: item.planParts && item.planParts.length > 0,
+          parent: parentId,
+          open: level < 1,
+          show: level < 2,
+          level: level,
+        });
+      }
+
+      const date = moment(item["planned-start-time"]);
+      const endDate = moment(item["planned-end-time"]);
+      items.push({
+        id: items.length + 1,
+        group: groupsMap.get(item.resource.id).id,
+        title: item.title,
+        start: date,
+        end: endDate,
+        className: 'className',
+      })
+
+      if (item.planParts && item.planParts.length > 0) {
+        this.buildData(item.planParts, groupsMap, items, level + 1, groupsMap.get(item.resource.id).id);
+      }
+    }
+  }
+
   constructor(props) {
     super(props);
 
     const data = require('./../data.json');
+
     const items = [];
-
     const groupsMap = new Map();
+    this.buildData(data, groupsMap, items, 0, null);
 
-    for (const item of data) {
+    /*let items = [];
+
+    let groupsMap = new Map();
+
+    const item = data
+    groupsMap.set(item.id, {
+      id: item.id,
+      title: item.resource ? item.resource.title : '',
+      rightTitle: 'right title',
+      hasChildren: item.planParts.length > 0,
+      parent: null,
+      open: true,
+      show: true,
+      level: 0
+    });
+
+    const date = moment(item["planned-start-time"]);
+    const endDate = moment(item["planned-end-time"]);
+    items.push({
+      id: items.length + 1,
+      group: groupsMap.get(item.id).id,
+      title: item.title,
+      start: date,
+      end: endDate,
+      className: 'className',
+    });*/
+
+    /*for (const item2 of data.planParts) {
+      groupsMap.set(item2.id, {
+        id: item2.id,
+        title: item2.resource ? item2.resource.title : '',
+        rightTitle: 'right title',
+        hasChildren: item2.planParts.length > 0,
+        parent: item.id,
+        open: false,
+        show: true,
+        level: 1
+      });
+
+      const date = moment(item2["planned-start-time"]);
+      const endDate = moment(item2["planned-end-time"]);
+      items.push({
+        id: items.length + 1,
+        group: groupsMap.get(item2.id).id,
+        title: item2.title,
+        start: date,
+        end: endDate,
+        className: 'className',
+      });
+
+      if (item2.planParts.length > 0) {
+        for (const item3 of item2.planParts) {
+          groupsMap.set(item3.id, {
+            id: item3.id,
+            title: item3.resource ? item3.resource.title : '',
+            rightTitle: 'right title',
+            hasChildren: item3.planParts.length > 0,
+            parent: item2.id,
+            open: false,
+            show: false,
+            level: 2
+          });
+
+          const date = moment(item3["planned-start-time"]);
+          const endDate = moment(item3["planned-end-time"]);
+          items.push({
+            id: items.length + 1,
+            group: groupsMap.get(item3.id).id,
+            title: item3.title,
+            start: date,
+            end: endDate,
+            className: 'className',
+          });
+
+          if (item3.planParts.length > 0) {
+            for (const item4 of item3.planParts) {
+              groupsMap.set(item4.id, {
+                id: item4.id,
+                title: item4.resource ? item4.resource.title : '',
+                rightTitle: 'right title',
+                hasChildren: item4.planParts.length > 0,
+                parent: item3.id,
+                open: false,
+                show: false,
+                level: 3
+              });
+
+              const date = moment(item4["planned-start-time"]);
+              const endDate = moment(item4["planned-end-time"]);
+              items.push({
+                id: items.length + 1,
+                group: groupsMap.get(item4.id).id,
+                title: item4.title,
+                start: date,
+                end: endDate,
+                className: 'className',
+              });
+
+
+              if (item4.planParts.length > 0) {
+                for (const item5 of item4.planParts) {
+                  groupsMap.set(item5.id, {
+                    id: item5.id,
+                    title: item5.resource ? item5.resource.title : '',
+                    rightTitle: 'right title',
+                    hasChildren: false,
+                    parent: item4.id,
+                    open: false,
+                    show: false,
+                    level: 4
+                  });
+
+                  const date = moment(item5["start-time"]);
+                  const endDate = moment(item5["end-time"]);
+                  items.push({
+                    id: items.length + 1,
+                    group: groupsMap.get(item5.id).id,
+                    title: item5.title,
+                    start: date,
+                    end: endDate,
+                    className: 'className',
+                  });
+                }
+              }
+            }
+          }
+        }
+      }
+    }*/
+
+    /*for (const item of data) {
       const groupColor = Math.floor(Math.random() * 16777215).toString(16);
 
       let hasParent = false
@@ -53,12 +217,12 @@ export default class PlanningTool extends Component {
         const endDate = moment(item['planned-start-time']).add(item['planned-duration'], 'hour');
         const bgColor = () => {
           switch (event['task-category']) {
-           /* case 'task_card':
+           /!* case 'task_card':
               return '#aa0000';
             case 'scheduled_wo':
               return '#00aa00';
             case 'maintenance_wo':
-              return '#0000aa';*/
+              return '#0000aa';*!/
             default:
               return '#' + groupColor;
           }
@@ -78,10 +242,20 @@ export default class PlanningTool extends Component {
           }
         });
       });
-    }
+    }*/
 
-    const groups = Array.from(groupsMap, ([key, values]) => values);
-    const defaultTimeStart = moment(data[0]['planned-start-time']).add(-12, 'hour');
+
+    let groups = Array.from(groupsMap, ([key, values]) => values);
+
+    groups = groups.reduce((accumulator, currentValue) => {
+      let item = accumulator.find(x => x.id === currentValue.parent);
+      let index = accumulator.indexOf(item);
+      index = index !== -1 ? index + 1 : accumulator.length;
+      accumulator.splice(index, 0, currentValue);
+      return accumulator;
+    }, []);
+
+    const defaultTimeStart = moment(items[0].start).add(-12, 'hour');
     const defaultTimeEnd = defaultTimeStart.clone().add(7, 'day');
 
     this.state = {
@@ -95,7 +269,7 @@ export default class PlanningTool extends Component {
   handleItemMove = (itemId, dragTime, newGroupOrder) => {
     const {items, groups} = this.state;
 
-    const group = groups[newGroupOrder];
+    const group = groups.filter(g => g.show)[newGroupOrder];
 
     this.setState({
       items: items.map(item =>
@@ -109,7 +283,7 @@ export default class PlanningTool extends Component {
       )
     });
 
-    console.log("Moved", itemId, dragTime, newGroupOrder);
+    // console.log("Moved", itemId, dragTime, newGroupOrder);
   };
 
   handleItemResize = (itemId, time, edge) => {
@@ -169,11 +343,11 @@ export default class PlanningTool extends Component {
       return Object.assign({}, group, {
         title: group.hasChildren ? (
           <div onClick={() => this.toggleGroup(group.id)}
-               style={{cursor: 'pointer', paddingLeft: group.parent !== null ? 20 : 0}}>
+               style={{cursor: 'pointer', paddingLeft: group.level * 20}}>
             {group.open ? '[-]' : '[+]'} {group.title}
           </div>
         ) : (
-          <div style={{paddingLeft: 40}}>{group.title}</div>
+          <div style={{paddingLeft: group.level * 20}}>{group.title}</div>
         )
       })
     });
@@ -190,6 +364,7 @@ export default class PlanningTool extends Component {
           itemHeightRatio={0.75}
           canMove={true}
           canResize={"both"}
+          sidebarWidth={250}
           defaultTimeStart={defaultTimeStart}
           defaultTimeEnd={defaultTimeEnd}
           onItemMove={this.handleItemMove}
